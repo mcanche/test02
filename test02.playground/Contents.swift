1df2946b91edf859e070c3387768f019982c2e76
mcanche <mcanche@gmail.com>
2015-06-12 18:17:38 -0500

    test02
    playground - tutorial 02

---
+++ test02.playground/Contents.swift
@@ -0,0 +1,21 @@
+// +-------------+
+// | video 2     |
+// +-------------+
+
+
+
+import UIKit
+
+var teams = ["heros","mostrers","seekers","red","blue"]
+
+
+for team in teams{
+    println(team)
+
+}
+
+for (var i = 0; i < 3; ++i){
+    println(teams[i])
+    
+}
+
